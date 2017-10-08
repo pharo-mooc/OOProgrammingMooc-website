5966818b46c3726d5429905f9b3f37dc0315edc8
StephaneDucasse <stephane.ducasse@inria.fr>
2017-10-08 17:35:23 +0200

    ready to push

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo Uploading files to files.pharo.org...
-rsync --progress --cvs-exclude --recursive --links --times --delete . filepharosync@files.pharo.org:/appli/files.pharo.org/moop
+rsync --progress --cvs-exclude --recursive --links --times --delete . filepharosync@files.pharo.org:/appli/files.pharo.org/lectures
 echo done
